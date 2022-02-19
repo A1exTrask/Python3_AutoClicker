70587efdefc1bbb977ad49445563c78a01d3acbc
AlexTrask <Leha2105@yandex.ru>
2022-02-19 18:45:15 +0300

    1

--- Auto Clicker.py
+++ Auto Clicker.py
@@ -2,7 +2,13 @@ from tkinter import *
 
 root = Tk()
 root.title('Auto Clicker')
-root.geometry('230x125')
+root.geometry('230x110')
 root.resizable(width=False, height=False)
 
+main_menu = Menu()
+main_menu.add_cascade(label="File")
+main_menu.add_cascade(label="Options")
+main_menu.add_cascade(label="Help")
+
+root.config(menu=main_menu)
 root.mainloop()
