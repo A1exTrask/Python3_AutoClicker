b0326d729d43821c5807a399f7aef5d7b2b58c07
AlexTrask <Leha2105@yandex.ru>
2022-02-19 19:23:35 +0300

    1

--- Auto Clicker.py
+++ Auto Clicker.py
@@ -6,9 +6,21 @@ root.geometry('230x110')
 root.resizable(width=False, height=False)
 
 main_menu = Menu()
-main_menu.add_cascade(label="File")
-main_menu.add_cascade(label="Options")
-main_menu.add_cascade(label="Help")
+
+file_menu = Menu(tearoff=0)
+file_menu.add_command(label="Exit")
+
+Options_menu = Menu(tearoff=0)
+Options_menu.add_command(label="1")
+Options_menu.add_command(label="2")
+Options_menu.add_command(label="3")
+
+Help_menu = Menu(tearoff=0)
+Help_menu.add_command(label="About")
+
+main_menu.add_cascade(label="File", menu=file_menu)
+main_menu.add_cascade(label="Options", menu=Options_menu)
+main_menu.add_cascade(label="Help", menu=Help_menu)
 
 root.config(menu=main_menu)
 root.mainloop()
